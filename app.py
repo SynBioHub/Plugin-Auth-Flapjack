9c2dcfd0a3eda10e3c3d83903bccec09e124f3ff
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-09-24 12:35:04 -0600

    changed template into flapjack specific plugin

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
-from flask import Flask, request, abort
-import os
-import sys
-import traceback
+from flask import Flask
 
+# to create an account to use with testing go to:
+# http://flapjack.rudge-lab.org/authentication?initialTab=signup
 
 app = Flask(__name__)
-
 
 @app.route("/status")
 def status():
     return("The Authorisation Test Plugin Flask Server is up and running")
 
 
-@app.route("/run", methods=["GET"])
-def run():
+@app.route("/login", methods=["GET"])
+def login():
     resp = {}
 
     # ~~~~~~~~~~~~ REPLACE THIS SECTION WITH OWN RUN CODE ~~~~~~~~~~~~~~~~~~~
-    login_url = "www.examples.org"
-    login_data = {'email': '<email>', 'username':'<username>','password' : '<password>'}
-    login_headers = {'Accept': 'text/plain'}
-    login_params = {'email':{'type': 'email', 'description': 'This is the email used for login', 'options': [], 'default': [], 'restrictions': {}},
-                    'password': {'type': 'password', 'description': 'This is your password', 'options': [], 'default': [], 'restrictions': {}},
-                    'username': {'type': 'text', 'description': 'This is the username for yoru account', 'options': [], 'default': [], 'restrictions': {}}
+    login_url = "http://flapjack.rudge-lab.org:8000/api/auth/log_in/"
+    login_data = {'username':'<username>','password' : '<password>'}
+    login_headers = {}
+    login_params = {
+                    'username': {'type': 'text', 'description': 'This is the username for yoru account', 'options': [], 'default': [], 'restrictions': {}},
+                    'password': {'type': 'password', 'description': 'This is your password', 'options': [], 'default': [], 'restrictions': {}}
                    }
+    token_params = {'access':"access", 'refresh':'refresh'}
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~ END SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     resp['login_url'] = login_url
     resp['login_data'] = login_data
     resp['login_headers'] = login_headers
     resp['login_parameters'] = login_params
+    resp['token_parameters'] = token_params
+    return resp
+
+@app.route("/refresh", methods=["GET"])
+def refresh():
+    resp = {}
+
+    # ~~~~~~~~~~~~ REPLACE THIS SECTION WITH OWN RUN CODE ~~~~~~~~~~~~~~~~~~~
+    login_url = "http://flapjack.rudge-lab.org:8000/api/auth/refresh/"
+    login_data = {'refresh':'<refresh>'}
+    login_headers = {}
+    token_params = {'access':"access"}
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~ END SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    resp['login_url'] = login_url
+    resp['login_data'] = login_data
+    resp['login_headers'] = login_headers
+    resp['token_parameters'] = token_params
     return resp
