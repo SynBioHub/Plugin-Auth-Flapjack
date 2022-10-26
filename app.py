b18565919850a6580b19ad8ab00911f7a58770d8
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-10-26 16:53:01 -0600

    update refresh endpoint and add logout endpoint

--- app.py
+++ app.py
@@ -32,21 +32,48 @@ def login():
     resp['token_parameters'] = token_params
     return resp
 
+
 @app.route("/refresh", methods=["GET"])
 def refresh():
     resp = {}
 
     # ~~~~~~~~~~~~ REPLACE THIS SECTION WITH OWN RUN CODE ~~~~~~~~~~~~~~~~~~~
-    login_url = "http://flapjack.rudge-lab.org:8000/api/auth/refresh/"
-    login_data = {'refresh':'<refresh>'}
-    login_headers = {}
+    refresh_exists = True
+    refresh_url = "http://flapjack.rudge-lab.org:8000/api/auth/refresh/"
+    refresh_data = {'refresh_token':'<refresh>'}
+    refresh_headers = {}
     refresh_token_name = 'refresh'
     token_params = {'access':"access"}
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~ END SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    resp['login_url'] = login_url
-    resp['login_data'] = login_data
-    resp['login_headers'] = login_headers
-    resp['refresh_token_name'] = refresh_token_name
-    resp['token_parameters'] = token_params
-    return resp
+    if refresh_exists:
+        resp['refresh_url'] = refresh_url
+        resp['refresh_data'] = refresh_data
+        resp['refresh_headers'] = refresh_headers
+        resp['refresh_token_name'] = refresh_token_name
+        resp['token_parameters'] = token_params
+        return resp
+    else:
+        return 'This login does not provide refresh tokens', 503
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    resp = {}
+
+    # ~~~~~~~~~~~~ REPLACE THIS SECTION WITH OWN RUN CODE ~~~~~~~~~~~~~~~~~~~
+    logout_exists = False
+    # logout_url = "https://authtesting.synbiohub.org/logoutAPI"
+    # logout_data = {'login_token':'<login_token>'}
+    # logout_headers = {}
+    # login_token_name = 'login_token'
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~ END SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    if logout_exists:
+        resp['logout_url'] = logout_url
+        resp['logout_data'] = logout_data
+        resp['logout_headers'] = logout_headers
+        resp['login_token_name'] = login_token_name
+        return resp
+    else:
+        return 'This login does not provide a logout endpoint', 503
